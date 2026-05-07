9553986c330cde097391ce1527576f823c261847
khalil <khalilbenmeziane@gmail.com>
2026-05-07 12:04:54 +0100

    update/useGeo hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useGeo } from "@/context/GeoProvider";
 
 export default function Home() {
+  const { country, latitude, longitude } = useGeo();
   return (
     <div>
       <SidebarTrigger />
-      home
+      <p>Country: {country}</p>
+      <p>Latitude: {latitude}</p>
+      <p>Longitude: {longitude}</p>
     </div>
   );
 }
